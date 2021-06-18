8559b7956f394e14236932738b875e1bd2df3c0e
Treml <clarissa.treml@stud.fh-campuswien.ac.at>
2021-06-18 10:17:43 +0200

    booking table continue

---
+++ test.js
@@ -0,0 +1,8 @@
+
+    var date = new Date ();
+    var tdate = date.getDate();
+    var month = date.getMonth();
+    var year = date.getUTCFullYear();
+
+    var minDate = year + "-" + month + "-" + tdate;
+    document.getElementById("calendar").setAttribute('min', minDate);
